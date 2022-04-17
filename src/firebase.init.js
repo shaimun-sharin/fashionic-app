ef3b6805973bf6a93098e46d1a71ca17f86aa659
shaimun-sharin <shaimunsharin@gmail.com>
2022-04-17 15:44:02 +0600

    initial commit

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuc4LMLhoEIDMYdamV8oj7SMJOl01119s",
+  authDomain: "fashionic-app.firebaseapp.com",
+  projectId: "fashionic-app",
+  storageBucket: "fashionic-app.appspot.com",
+  messagingSenderId: "504877892276",
+  appId: "1:504877892276:web:ebd7fd3920db31f3a8cde1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
